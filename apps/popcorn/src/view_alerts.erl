1452dcd436f923098b58cc6b434be296ed2936b5
Brujo Benavides <elbrujohalcon@inaka.net>
2013-01-28 16:33:07 -0300

    Severity filter

--- apps/popcorn/src/view_alerts.erl
+++ apps/popcorn/src/view_alerts.erl
@@ -3,7 +3,7 @@
 
 -include("include/popcorn.hrl").
 
--export([head_includes/0, alerts/1, username/0, header_button/1]).
+-export([head_includes/0, alerts/1, username/0, known_severities/1, header_button/1]).
 
 -spec head_includes() -> list().
 head_includes() -> popcorn_util:head_includes().
@@ -19,3 +19,11 @@ header_button(Context) ->
 
 -spec username() -> string().
 username() -> "admin".
+
+-spec known_severities(dict()) -> list().
+known_severities(_) ->
+    lists:map(fun({Severity_Name, Severity_Number}) ->
+        Params = [{'label',        Severity_Name},
+                  {'severity_num', integer_to_list(Severity_Number)}],
+        dict:from_list(Params)
+      end, lists:reverse(popcorn_util:all_severities())).
