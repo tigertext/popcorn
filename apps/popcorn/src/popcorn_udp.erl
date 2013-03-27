878627ea6edcd770db6cbad98f6657d37243aad4
Marc Campbell <marc.e.campbell@gmail.com>
2013-03-27 11:12:30 -0700

    updates

--- apps/popcorn/src/popcorn_udp.erl
+++ apps/popcorn/src/popcorn_udp.erl
@@ -61,9 +61,9 @@ handle_info({broadcast, {new_storage_workers, Workers}}, State) ->
 handle_info({udp, Socket, _Host, _Port, Bin}, #state{workers = Workers} = State) ->
     ?RPS_INCREMENT(udp_received),
     {Popcorn_Node, Log_Message} = decode_protobuffs_message(State#state.retention_policy, Bin),
-            New_State = ingest_packet(State, Popcorn_Node, Log_Message, Workers),
-            inet:setopts(Socket, [{active, once}]),
-            {noreply, New_State};
+    New_State = ingest_packet(State, Popcorn_Node, Log_Message, Workers),
+    inet:setopts(Socket, [{active, once}]),
+    {noreply, New_State};
 
 handle_info(timeout, State) ->
     {noreply, State};
@@ -93,6 +93,8 @@ decode_protobuffs_message(Retention_Policy, Encoded_Message) ->
     case Packet_Version of
         1 -> 
             decode_protobuffs_message(Retention_Policy, 1, Rest);
+        2 ->
+            decode_protobuffs_message(Retention_Policy, 2, Rest);
         No_Version when is_binary(No_Version) ->
             decode_protobuffs_message(Retention_Policy, 0, Encoded_Message);
         Other ->
@@ -189,7 +191,56 @@ decode_protobuffs_message(Retention_Policy, 1, Rest) ->
                                 log_line     = check_undefined(Line2),
                                 log_pid      = check_undefined(Pid)},
 
+    {Popcorn_Node, Log_Message};
+
+decode_protobuffs_message(Retention_Policy, 2, Rest) ->
+    {{2, Node}, Rest1} = protobuffs:decode(Rest, bytes),
+    {{3, Node_Role}, Rest2} = protobuffs:decode(Rest1, bytes),
+    {{4, Node_Version}, Rest3} = protobuffs:decode(Rest2, bytes),
+    {{5, Severity}, Rest4} = protobuffs:decode(Rest3, bytes),
+    {{6, Message}, Rest5} = protobuffs:decode(Rest4, bytes),
+    {{7, Module}, Rest6} = protobuffs:decode(Rest5, bytes),
+    {{8, Function}, Rest7} = protobuffs:decode(Rest6, bytes),
+    {{9, Line}, Rest8} = protobuffs:decode(Rest7, bytes),
+    {{10, Pid}, Rest9} = protobuffs:decode(Rest8, bytes),
+    {{11, Timestamp}, <<>>} = protobuffs:decode(Rest9, float),
+
+    {Topics, Identities} = get_tags(binary_to_list(Message)),
+
+    Popcorn_Severity = case check_undefined(Severity) of
+                           undefined -> popcorn_util:severity_to_number(none);
+                           _         -> Severity
+                       end,
+
+    Time_To_Expire = case proplists:get_value(Popcorn_Severity, Retention_Policy) of
+                         undefined -> 7200000000;
+                         TTL       -> TTL
+                     end,
+
+    Popcorn_Node = #popcorn_node{node_name = check_undefined(Node),
+                                 role      = check_undefined(Node_Role),
+                                 version   = check_undefined(Node_Version)},
+
+    %% Ensure we have a module and line so we can perform rollup
+    {Module2, Line2} = location_check(Module, Line, Message),
+
+    Log_Message  = #log_message{message_id   = ?PU:unique_id(),
+                                timestamp    = Timestamp,
+                                expire_at    = Timestamp + Time_To_Expire,
+                                severity     = Popcorn_Severity,
+                                message      = check_undefined(Message),
+                                topics       = Topics,
+                                identities   = Identities,
+                                log_nodename = Popcorn_Node#popcorn_node.node_name,
+                                log_product  = Popcorn_Node#popcorn_node.role,
+                                log_version  = Popcorn_Node#popcorn_node.version,
+                                log_module   = check_undefined(Module2),
+                                log_function = check_undefined(Function),
+                                log_line     = check_undefined(Line2),
+                                log_pid      = check_undefined(Pid)},
+
     {Popcorn_Node, Log_Message}.
+
 
 check_undefined(<<>>) -> undefined;
 check_undefined(Value) -> Value.
